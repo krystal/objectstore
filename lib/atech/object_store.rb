95828e2bac843c1768463654b779b4be52b507d3
Adam Cooke <adam@atechmedia.com>
2012-06-14 10:36:32 +0100

    bump version

--- lib/atech/object_store.rb
+++ lib/atech/object_store.rb
@@ -1,6 +1,6 @@
 module Atech
   module ObjectStore
-    VERSION = '0.0.1'
+    VERSION = '1.0.0'
     
     ## Error class which all Object Store errors are inherited fro
     class Error < StandardError; end
